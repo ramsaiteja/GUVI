a64c4d192018deec3005ff6f20df68c99bf4f2a9
ramsaiteja <41988142+ramsaiteja@users.noreply.github.com>
2018-08-01 05:11:04 -0400

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Even
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int s=sc.nextInt();
+		if(s%2==0)
+		{
+			System.out.println("Even");
+		}
+		else if(s%2!=0)
+		{
+			System.out.println("invalid");
+		}
+	}
+}
