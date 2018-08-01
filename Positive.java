9fa80cc047f4008b039c500772e6e9835d1aa1d7
ramsaiteja <41988142+ramsaiteja@users.noreply.github.com>
2018-08-01 04:50:30 -0400

    Create Positive.java

---
+++ Positive.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Positive
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int s=sc.nextInt();
+		if(s>0)
+		{
+			System.out.println("Positive");
+		}
+		else
+		{
+			System.out.println("Zero");
+		}
+	}
+}
