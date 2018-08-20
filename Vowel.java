a588c3930c061fd0f9407c47c078f2cf83de0675
ramsaiteja <41988142+ramsaiteja@users.noreply.github.com>
2018-08-20 12:52:10 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Vowel
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		char s=sc.next().charAt(0);
+		if(s=='a'|| s=='e'|| s=='i'|| s=='o'|| s=='u')
+		{
+			System.out.println("Vowels");
+		}
+		else if(s=='b'||s=='c'|| s=='d'|| s=='f'|| s=='g'|| s=='h' ||s=='j' || s=='k'||s=='l'|| s=='m'|| s=='n'|| s=='p'|| s=='q'||s=='r'||s=='s'||s=='t'||s=='w'||s=='x'||s=='y'||s=='z')
+		{
+			System.out.println("Consonents");
+		}
+		else
+		{
+			System.out.println("invalid");
+		}
+	}
+}
