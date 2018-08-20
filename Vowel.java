6ebf9c53dea80fa747d80ccdfd01dce8c6a8a0da
ramsaiteja <41988142+ramsaiteja@users.noreply.github.com>
2018-08-20 12:55:58 +0530

    Update Vowel.java

--- Vowel.java
+++ Vowel.java
@@ -9,7 +9,7 @@ class Vowel
 		char s=sc.next().charAt(0);
 		if(s=='a'|| s=='e'|| s=='i'|| s=='o'|| s=='u')
 		{
-			System.out.println("Vowels");
+			System.out.println("Vowel");
 		}
 		else if(s=='b'||s=='c'|| s=='d'|| s=='f'|| s=='g'|| s=='h' ||s=='j' || s=='k'||s=='l'|| s=='m'|| s=='n'|| s=='p'|| s=='q'||s=='r'||s=='s'||s=='t'||s=='w'||s=='x'||s=='y'||s=='z')
 		{
